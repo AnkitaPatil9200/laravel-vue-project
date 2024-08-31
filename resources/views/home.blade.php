70ba50bd3a777637cd7f5663ef17435605c4eeb7
AnkitaPatil9200 <mohiteankita920@gmail.com>
2024-08-31 13:21:32 +0530

    basic setup doe

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Sortable UI jquery -->
+    <!-- <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/themes/smoothness/jquery-ui.css" /> -->
+    <!-- CSS Global Compulsory -->
+    <!-- <link rel="stylesheet" href="{{asset('admin/css/bootstrap.min.css')}}"> -->
+</head>
+
+<body>
+<h1>Hi there</h1>
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        alert();
+    });
+</script>
+</html>
